74d73f63fbfb0b3aed95594ed04afcb7af0199b2
TaumuLu <972409545@qq.com>
2017-01-10 18:17:44 +0800

    blog

---
+++ Learn/microblog/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/hello', function(req, res, next) {
+  res.send('hello');
+});
+module.exports = router;
